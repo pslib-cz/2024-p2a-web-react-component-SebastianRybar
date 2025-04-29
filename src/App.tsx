f8e6d906c9f5a852554098c4df58bdf9e9e4cbc3
Sebastain Rybar <sebastinan.rybar.023@pslib.cz>
2025-04-29 12:55:34 +0200

    minuly tyden

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,58 @@
+import './App.tsx'
+import ViewBox from './components/PercentView.tsx'
+// type Address ={
+//     street: string,
+//     city: string,
+//     country: string
+// }
 
-export const App = () => 
-(
-    <p>Ahoj světe</p>
+// type MyType = {
+//     name: string,
+//     age: number,
+//     doIt: (h: number) => number,
+//     isAlive: boolean,
+//     hobbies: Array<string>,
+//     address?: Address
+// }
 
-);
+// const myPerson: MyType = {
+//     name: 'John',
+//     age: 30,
+//     doIt: (h: number) => h + 1,
+//     isAlive: true,
+//     hobbies: ['reading', 'gaming'],
+//     address: {
+//         street: '123 Main St',
+//         city: 'New York',
+//         country: 'USA'
+//     }
+// }
+// type Arrowtype = (x: number) => number
+
+// const arrow1 = (x):number => x + 1
+
+// const arrow2 = (x):number => (
+//     x + 1
+// )
+
+// const arrow3 = (x):number => {
+//     x = 2;
+//     return(
+//         x + 1
+//     )
+    
+// }
+
+const App = () => {
+    let cislo: number = 34;
+    cislo -= 1;
+
+    return(
+        <>
+           <ViewBox value={cislo + 1} max={100} makeColor = {(p) => "red"} />
+           <ViewBox value={cislo + 1} max={200} makeColor = {(p) => "red"} />
+           <ViewBox value={30} max={300} makeColor = {(p) => 'rgb(${p*2*100},255,255)'} />
+        </>
+    )
+}
+export default App
--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,8 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client';
-import {App} from './App'
+import  App  from './App';
 
-createRoot(document.querySelector('#root')!).render(
+createRoot(document.querySelector("#root")).render(
   <StrictMode>
     <App />
   </StrictMode>,
